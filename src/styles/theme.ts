0d4d7e560e4dc4965f1bf864c2a256247192516b
João Melo <joao.melo@coinbase.com>
2021-10-16 20:58:02 -0300

    chore: move the project structure to the src folder

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   fonts: {
-    heading: "Roboto",
-    body: "Roboto",
+    heading: "Poppins",
+    body: "Poppins",
   },
 });
