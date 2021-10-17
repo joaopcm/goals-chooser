ecde9826debaa2e1b5eb5335d7d890e368d9e0b9
João Melo <joao.melo@coinbase.com>
2021-10-16 22:13:38 -0300

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Box, Flex, Heading } from '@chakra-ui/react'
+
+export default function Header() {
+  return (
+    <Box
+      as="header"
+      height="5rem"
+      boxShadow="xl"
+      backgroundColor="cyan.500"
+      color="white"
+    >
+      <Flex
+        maxWidth="1120px"
+        height="5rem"
+        marginRight="auto"
+        marginLeft="auto"
+        paddingRight="2rem"
+        paddingLeft="2rem"
+        alignItems="center"
+      >
+        <Heading
+          fontSize="3xl"
+          fontWeight="bold"
+          letterSpacing="tight"
+          width="64"
+        >
+          What's next?
+        </Heading>
+      </Flex>
+    </Box>
+  )
+}
